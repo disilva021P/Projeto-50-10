e26a62b03bcde609de03fde0dc0fda1e135a9d65
disilva021 <disilva.profissional@gmail.com>
2026-02-14 12:44:41 +0000

    Estrutura Base

---
+++ SiteEntArtes/src/main/java/ipcaProjeto50/_Grupo62026/SiteEntArtes/SiteEntArtesApplication.java
@@ -0,0 +1,13 @@
+package ipcaProjeto50._Grupo62026.SiteEntArtes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteEntArtesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteEntArtesApplication.class, args);
+	}
+
+}
---
+++ SiteEntArtes/src/test/java/ipcaProjeto50/_Grupo62026/SiteEntArtes/SiteEntArtesApplicationTests.java
@@ -0,0 +1,13 @@
+package ipcaProjeto50._Grupo62026.SiteEntArtes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteEntArtesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
