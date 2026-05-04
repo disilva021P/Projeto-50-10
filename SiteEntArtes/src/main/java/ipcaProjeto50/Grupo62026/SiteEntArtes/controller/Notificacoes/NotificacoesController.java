4e8b181cbfc8ec47013c02173c1e95b4b38a084d
R0DRAPT <152292111+R0DRAPT@users.noreply.github.com>
2026-05-04 19:20:09 +0100

    Testes Controller Marketplace, Inventario, Mensagens, Grupos e Notificações

--- SiteEntArtes/src/main/java/ipcaProjeto50/Grupo62026/SiteEntArtes/controller/Notificacoes/NotificacoesController.java
+++ SiteEntArtes/src/main/java/ipcaProjeto50/Grupo62026/SiteEntArtes/controller/Notificacoes/NotificacoesController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/notificacoes")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*") // Adiciona esta linha para testar
+@CrossOrigin(origins = "*")
 public class NotificacoesController {
     private final NotificacoesService notificacoesService;
 
